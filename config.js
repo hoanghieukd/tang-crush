8451718714c0b60a75fc28831064847c499e9c70
hoanghieukd <86854164+hoanghieukd@users.noreply.github.com>
2021-07-03 12:45:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Phương à!',
-    introDesc: `Trái đất vốn lạ thường
-    Trời cho bốn phương tám hướng
-    Mà tớ chỉ thích 1 phương 1 hướng
+    introDesc: `Trái đất vốn lạ thường.   
+    Trời cho bốn phương tám hướng  . 
+    Mà tớ chỉ thích 1 phương 1 hướng   .
     Là hướng về trái tim phương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Tớ đồng ý làm ny cậu <33',
+    btnNo: 'Không, mình chỉ làm bạn :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao t lại yêu c <3',
+    btnReply: 'Gửi cho hiếu  <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h tớ qua đón nhé công chúa.',
+    mess: 'Tớ biết mà 🥰. Yêu c nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h tớ qua đón c nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://m.me/oscar.hieu.12' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
